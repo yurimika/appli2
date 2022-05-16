fa8c88bf74ac289f8408542665e76a361b3250db
EC2 Default User <ec2-user@ip-172-31-80-47.ec2.internal>
2022-05-16 18:10:32 +0000

    finish 4-22

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,7 +1,13 @@
 class PostImage < ApplicationRecord
-  has_one_attached :image  
+  has_one_attached :image
   belongs_to :user
-   has_many :post_comments, dependent: :destroy
+  has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+
+  validates :shop_name, presence: true
+  validates :image, presence: true
+
+
    def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
@@ -9,4 +15,9 @@ class PostImage < ApplicationRecord
     end
     image
    end
+
+    def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :post_images, dependent: :destroy
    has_many :post_comments, dependent: :destroy
   has_one_attached :profile_image
-  
+    has_many :favorites, dependent: :destroy
+
   def get_profile_image(width, height)
     unless profile_image.attached?
       file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
@@ -15,5 +16,5 @@ class User < ApplicationRecord
     end
     profile_image.variant(resize_to_limit: [100, 100]).processed
   end
-  
+
 end
